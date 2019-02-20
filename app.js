bee989959111c61a7b92270dbc219a9e1023bafd
RJ Hsiao <rj19860725@gmail.com>
2019-02-20 10:07:14 +0800

    Add public pages for testing

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
 app.use(logger('short'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
